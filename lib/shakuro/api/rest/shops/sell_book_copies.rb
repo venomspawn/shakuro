0c847c81ca9ef39db7c24d36e96606fa307d6a71
Alexander Ilchukov <alex.ilchukov@gmail.com>
2018-08-24 11:24:54 +0300

    #7 Add REST API method of marking of book copies as sold

---
+++ lib/shakuro/api/rest/shops/sell_book_copies.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Shakuro
+  module API
+    module REST
+      # Provides definitions of REST API methods which operate by information
+      # of shops
+      module Shops
+        # Provides definition of REST API method which marks book copies of a
+        # shop as sold
+        module SellBookCopies
+          # Registers REST API method in REST API controller
+          # @param [Shakuro::API::REST::Controller] controller
+          #   REST API controller
+          def self.registered(controller)
+            # Marks book copies of a shop as sold
+            # @param [Hash]
+            #   associative array with structure described in JSON-schema
+            #   {Shakuro::Actions::Publishers::IndexShop::PARAMS_SCHEMA}
+            # @return [Status]
+            #   204
+            controller.post '/shops/:id/sold_book_copies' do |id|
+              Actions::Shops.sell_book_copies(request.body, id: id)
+              status :no_content
+            end
+          end
+        end
+
+        Controller.register SellBookCopies
+      end
+    end
+  end
+end
